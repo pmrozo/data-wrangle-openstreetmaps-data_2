d2ea2f211dea3432922c1359f6e6c0b11a499c52
pmrozo <pawel.mrozowski@avioaero.it>
2015-04-15 15:37:13 +0200

    Committed by Udacity

---
+++ auditing/old/building.py
@@ -0,0 +1,42 @@
+"""
+Your task in this exercise has two steps:
+
+- audit the OSMFILE and change the variable 'mapping' to reflect the changes needed to fix 
+    the unexpected street types to the appropriate ones in the expected list.
+    You have to add mappings only for the actual problems you find in this OSMFILE,
+    not a generalized solution, since that may and will depend on the particular area you are auditing.
+- write the update_name function, to actually fix the street name.
+    The function takes a string with street name as an argument and should return the fixed name
+    We have provided a simple test so that you see what exactly is expected
+"""
+import xml.etree.cElementTree as ET
+from collections import defaultdict
+import re
+import pprint
+
+OSMFILE = "map.osm"
+
+
+def audit(osmfile):
+    osm_file = open(osmfile, "r")
+    street_types = defaultdict(set)
+    for event, elem in ET.iterparse(osm_file, events=("start",)):
+
+        if elem.tag == "node":
+            for tag in elem.iter("tag"):
+				if tag.attrib['k'] == "building":
+					print tag.tag, tag.attrib
+    
+
+
+
+def test():
+     audit(OSMFILE)
+   
+
+
+ 
+
+
+if __name__ == '__main__':
+    test()
